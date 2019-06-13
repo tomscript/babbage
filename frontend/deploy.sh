66e3da82a4238d04d6355c8a6c4b6a40306e5160
Tom Fitzgerald <tom3fitzgerald@gmail.com>
2019-06-12 21:11:21 -0700

    fe should deploy to new version not service

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,5 +1,4 @@
 ng build --prod
-gcloud config configurations activate babbage
-gcloud app deploy dist/frontend.yaml --promote --quiet --version=live
+gcloud app deploy dist/frontend.yaml --promote --quiet --version=live --project=babbage-stable
 # Copied so Jinja could access it. Get an error if it tires to pull from a static_dir.
 cp dist/public/index.html dist/
